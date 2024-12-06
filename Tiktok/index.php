cb07445ac16ecfa8f09013b8dc232fd44ed2d738
Brian Bryan <bryanandbrian2002@gmail.com>
2024-12-06 23:49:03 +0800

    add new css to calendar

--- Tiktok/index.php
+++ Tiktok/index.php
@@ -3487,7 +3487,7 @@ session_start();
             </div>
             <div class="date-right">
                 <div class="nov">
-                    <div class="nov-header">
+                    <div class="nov-header justify-content-start">
                         <button class="back-to-jan vi-icon-d-arrow-left"></button>
                         <button class="back-to-prev vi-icon-arrow-left"></button>
                         <span class="nov-txt">November 2024</span>
@@ -3503,12 +3503,12 @@ session_start();
                                 <th>Fri</th>
                                 <th>Sat</th>
                             </tr>
-                            <tr class="daynumber">
-                                <td>27</td>
-                                <td>28</td>
-                                <td>29</td>
-                                <td>30</td>
-                                <td>31</td>
+                            <tr>
+                                <td class="greyout">27</td>
+                                <td class="greyout">28</td>
+                                <td class="greyout">29</td>
+                                <td class="greyout">30</td>
+                                <td class="greyout">31</td>
                                 <td>1</td>
                                 <td>2</td>
                             </tr>
@@ -3549,19 +3549,19 @@ session_start();
                                 <td>30</td>
                             </tr>
                             <tr>
-                                <td>1</td>
-                                <td>2</td>
-                                <td>3</td>
-                                <td>4</td>
-                                <td>5</td>
-                                <td>6</td>
-                                <td>7</td>
+                                <td class="greyout">1</td>
+                                <td class="greyout">2</td>
+                                <td class="greyout">3</td>
+                                <td class="greyout">4</td>
+                                <td class="greyout">5</td>
+                                <td class="greyout">6</td>
+                                <td class="greyout">7</td>
                             </tr>
                         </tbody>
                     </table>
                 </div>
                 <div class="dec">
-                    <div class="dec-header">
+                    <div class="dec-header justify-content-end">
                         <span class="dec-txt">December 2024</span>
                         <button class="back-to-next vi-icon-arrow-right"></button>
                         <button class="back-to-dec vi-icon-d-arrow-right"></button>
@@ -3578,64 +3578,67 @@ session_start();
                                 <th>Sat</th>
                             </tr>
                             <tr>
-                                <td>27</td>
-                                <td>28</td>
-                                <td>29</td>
-                                <td>30</td>
-                                <td>31</td>
-                                <td>1</td>
-                                <td>2</td>
+                                <td class="greyout">24</td>
+                                <td class="greyout">25</td>
+                                <td class="greyout">26</td>
+                                <td class="greyout">27</td>
+                                <td class="greyout">28</td>
+                                <td class="greyout">29</td>
+                                <td class="greyout">30</td>
                             </tr>
                             <tr>
+                                <td>1</td>
+                                <td>2</td>
                                 <td>3</td>
                                 <td>4</td>
                                 <td>5</td>
                                 <td>6</td>
                                 <td>7</td>
-                                <td>8</td>
-                                <td>9</td>
                             </tr>
                             <tr>
+                                <td>8</td>
+                                <td>9</td>
                                 <td>10</td>
                                 <td>11</td>
                                 <td>12</td>
                                 <td>13</td>
                                 <td>14</td>
-                                <td>15</td>
-                                <td>16</td>
                             </tr>
                             <tr>
+                                <td>15</td>
+                                <td>16</td>
                                 <td>17</td>
                                 <td>18</td>
                                 <td>19</td>
                                 <td>20</td>
                                 <td>21</td>
-                                <td>22</td>
-                                <td>23</td>
                             </tr>
                             <tr>
+                                <td>22</td>
+                                <td>23</td>
                                 <td>24</td>
                                 <td>25</td>
                                 <td>26</td>
                                 <td>27</td>
                                 <td>28</td>
-                                <td>29</td>
-                                <td>30</td>
                             </tr>
                             <tr>
-                                <td>1</td>
-                                <td>2</td>
-                                <td>3</td>
-                                <td>4</td>
-                                <td>5</td>
-                                <td>6</td>
-                                <td>7</td>
+                                <td>29</td>
+                                <td>30</td>
+                                <td>31</td>
+                                <td class="greyout">1</td>
+                                <td class="greyout">2</td>
+                                <td class="greyout">3</td>
+                                <td class="greyout">4</td>
                             </tr>
                         </tbody>
                     </table>
                 </div>
-
             </div>
+        </div>
+        <div class="date-footer">
+            <button class="date-cancel">Cancel</button>
+            <button class="date-apply">Apply</button>
         </div>
     </div>
 
